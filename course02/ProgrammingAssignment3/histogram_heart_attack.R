16fea9a699654daab47c43269167010195ba9cac
Matteo Buscaroli <matteo@macmini.home>
2025-05-10 20:11:58 +0100

    Add comments to the histogram_heart_attack function

--- course02/ProgrammingAssignment3/histogram_heart_attack.R
+++ course02/ProgrammingAssignment3/histogram_heart_attack.R
@@ -1,16 +1,15 @@
 ## Print an histogram of the 30 day death rate for heart attack from the outcomes data set.
-## Needs importing "ggplot2"
+## Needs importing "dplyr" and "ggplot2" from the tidyverse
 histogram_heart_attack <- function() {
   # import the dataset
   df <- read_csv("./ProgrammingAssignment3/rprog_data_ProgAssignment3-data/outcome-of-care-measures.csv")
   
   print(head(df))
+  # rename the column related to the mortality from heart attack
   # convert the type of the desired column to numeric
   df <- df |> rename(heart_attack = colnames(df)[11])
-  
   df$heart_attack <- as.numeric(df$heart_attack)
 
-  
   # plot the histogram
   ggplot(df, aes(x = heart_attack)) +
     geom_histogram(binwidth = 0.5, fill="#DD0000", color="#000000") +
